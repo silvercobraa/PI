02bad141f35bc4a5e9cb45bf4a97f6e39536223a
scholtz8 <cristian.scholtz@gmail.com>
2018-10-02 20:03:56 -0300

    clase conexion agregada

---
+++ ProyectoPI/src/java/Connection/Conexion.java
@@ -0,0 +1,34 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    protected Connection conexion;
+    private final String JDBC_DRIVER = "org.postgresql.Driver";
+    private final String DB_URL = "jdbc:postgresql://plop.inf.udec.cl/bdi2017d";
+    private final String USER = "bdi2017d";
+    private final String PASS = "bdi2017d";
+    
+    public void conectar() throws Exception{
+        try{
+            conexion = DriverManager.getConnection(DB_URL,USER,PASS);
+            Class.forName(JDBC_DRIVER);          
+        }
+        catch(ClassNotFoundException | SQLException e){
+            throw e;
+        }
+    }
+    
+    public void desconectar() throws SQLException{
+        if(conexion != null){
+            if(!conexion.isClosed()){
+                conexion.close();
+            }
+        }
+    }
+
+
+    
+}
