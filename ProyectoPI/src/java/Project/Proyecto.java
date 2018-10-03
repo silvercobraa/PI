0fd4525e46f0b25d01a1ff50ff3da8060b234936
scholtz8 <cristian.scholtz@gmail.com>
2018-10-03 01:34:42 -0300

    clase departamento agregado metodo listar

--- ProyectoPI/src/java/Project/Proyecto.java
+++ ProyectoPI/src/java/Project/Proyecto.java
@@ -1,17 +1,17 @@
 package Project;
 
-import DAO.Departamento;
 import DAO.DepartamentoDAO;
 import DAO.DepartamentoDAOImpl;
         
 public class Proyecto {
     
     public static void main(String... mitocode){
-        Departamento dep;
+        
         try {
             DepartamentoDAO dao = new DepartamentoDAOImpl();
-            dep = dao.buscar("DIICC");
-            System.out.println(dep.getNombre());           
+            System.out.println(dao.buscar("DIICC"));            
+            System.out.println("////");            
+            System.out.println(dao.listar().iterator());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } 
