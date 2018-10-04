84d34bef2d62192f6f7928e3e00af95188f9e99e
scholtz8 <cristian.scholtz@gmail.com>
2018-10-03 23:21:16 -0300

    Agreado DAO depa,lugar

---
+++ ProyectoGit/src/java/modelo/DepartamentoDAOImpl.java
@@ -0,0 +1,60 @@
+package modelo;
+
+import controlador.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartamentoDAOImpl extends Conexion implements DepartamentoDAO {
+
+    @Override
+    public String buscar(String id_dep) throws Exception {
+        String nombre = null;
+        String sqlQuery = "SELECT * FROM proyecto.departamento WHERE id_depart =  '"+id_dep+"'  ;";        
+        PreparedStatement st = null;
+        try {
+            this.conectar();
+            st = this.conexion.prepareStatement(sqlQuery);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+            nombre = rs.getString("nombre");
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        finally {
+            if( st != null) {
+                st.close();
+            }
+            this.desconectar();
+        }
+        return nombre;        
+    }
+
+    @Override
+    public List<String> listar() throws Exception {
+        List<String> list = new ArrayList<String>();
+        String sqlQuery = "SELECT * FROM proyecto.departamento";
+        PreparedStatement st = null;
+        try {
+            this.conectar();
+            st = this.conexion.prepareStatement(sqlQuery);
+            ResultSet rs = st.executeQuery();            
+            while(rs.next()){
+                System.out.println(rs.getString("nombre"));
+                list.add(rs.getString("nombre"));
+            }
+            
+        } catch (Exception e) {
+            throw e;
+        }
+        finally {
+            if(st != null){
+                st.close();
+            }
+            this.desconectar();
+        }        
+        return list;
+    }    
+}
