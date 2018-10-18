e865ff6a6342d6e981aec27e6ad835d1e8bf8d90
César <silver.cobra1n@gmail.com>
2018-10-18 16:40:34 -0300

    Login

---
+++ ProyectoGit/src/java/controlador/Login2.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Conchetumare
+ */
+@WebServlet(name = "Login2", urlPatterns = {"/Login2.do"})
+public class Login2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\">");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login2</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Login2 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String usu = request.getParameter("usuario");
+        String pass = request.getParameter("password");
+        HttpSession session = request.getSession();
+        System.out.println(usu);
+        System.out.println(pass);
+        //deberíamos buscar el usuario en la base de datos, pero dado que se escapa de este tema, ponemos un ejemplo en el mismo código
+        if(usu.equals("admin") && pass.equals("admin") && session.getAttribute("usuario") == null){
+        // if(usu.equals("admin") && pass.equals("admin") && session != null && session.getAttribute("usuario") == null){
+            //si coincide usuario y password y además no hay sesión iniciada
+            session.setAttribute("usuario", usu);
+            //redirijo a página con información de login exitoso
+            response.sendRedirect("loginExito.jsp");
+        }else{
+            //lógica para login inválido
+            response.sendRedirect("loginFallo.jsp");
+        }   
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
