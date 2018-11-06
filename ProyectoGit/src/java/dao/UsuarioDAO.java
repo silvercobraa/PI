6c76d8837a67fa04d6757c17273f31499d1a8c5e
César <silver.cobra1n@gmail.com>
2018-11-06 17:24:01 -0300

    Ahora el login verifica las credenciales en la base de datos

--- ProyectoGit/src/java/dao/UsuarioDAO.java
+++ ProyectoGit/src/java/dao/UsuarioDAO.java
@@ -6,4 +6,5 @@ public interface UsuarioDAO {
     public ResultSet entregarDatos(String id) throws Exception;
     public ResultSet eventosSeguidos(String id) throws Exception;
     public ResultSet posiblesEventos(String id) throws Exception;
+    public Usuario buscarPorId(String id) throws Exception;
 }
